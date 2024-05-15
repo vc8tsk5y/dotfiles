4ca5f7d7824a4a52e181552ce1686c5ddf653155
Tim Zechmeister <tim.zechmeister03@gmail.com>
2024-05-15 17:42:08 +0200

    new background2

--- .config/rofi/scripts/everblush.sh
+++ .config/rofi/scripts/everblush.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 everblush() {
-    options=("all" "background0-#141b1e" "background1-#232a2d" "background2-#32393c" "red-#e57474" "green-#8ccf7e" "yellow-#e5c76b" "blue-#67b0e8" "magenta-#c47fd5" "cyan-#6cbfbf" "white0-#dadada" "white1-#b3b9b8" "white2-#8e979a")
+    options=("all" "background0-#141b1e" "background1-#232a2d" "background2-#2d3437" "red-#e57474" "green-#8ccf7e" "yellow-#e5c76b" "blue-#67b0e8" "magenta-#c47fd5" "cyan-#6cbfbf" "white0-#dadada" "white1-#b3b9b8" "white2-#8e979a")
     declare -i i=0
     selected=$(
         while [ $i -lt ${#options[@]} ]
@@ -11,13 +11,13 @@ everblush() {
         done | rofi -dmenu -p " everblush" -i)
 
     if [[ $selected == "all" ]]; then
-        echo -en "background0-#141b1e\nbackground1-#232a2d\nbackground2-#32393c\nred-#e57474\ngreen-#8ccf7e\nyellow-#e5c76b\nblue-#67b0e8\nmagenta-#c47fd5\ncyan-#6cbfbf\nwhite0-#dadada\nwhite1-#b3b9b8\nwhite2-#8e979a" | wl-copy
+        echo -en "background0-#141b1e\nbackground1-#232a2d\nbackground2-#2d3437\nred-#e57474\ngreen-#8ccf7e\nyellow-#e5c76b\nblue-#67b0e8\nmagenta-#c47fd5\ncyan-#6cbfbf\nwhite0-#dadada\nwhite1-#b3b9b8\nwhite2-#8e979a" | wl-copy
     elif [[ $selected == "background0-#141b1e" ]]; then
         echo -n 141b1e | wl-copy
     elif [[ $selected == "background1-#232a2d" ]]; then
         echo -n 232a2d | wl-copy
-    elif [[ $selected == "background2-#32393c" ]]; then
-        echo -n 32393c | wl-copy
+    elif [[ $selected == "background2-#2d3437" ]]; then
+        echo -n 2d3437 | wl-copy
     elif [[ $selected == "red-#e57474" ]]; then
         echo -n e57474 | wl-copy
     elif [[ $selected == "green-#8ccf7e" ]]; then
